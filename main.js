9759ef6aebcf0d8727572cfc3ee138ff50496cdc
bt <btfranzen@gmail.com>
2017-10-25 12:49:21 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// jQ ready statement start
+var nasaAPI = 'https://api.nasa.gov/planetary/apod?api_key=VonJt8Vve5LhNeSezacnaH3Fd7m0IPxnO5YI5Kr1'
+
+$(document).ready(function() {
+
+  $.get('https://apod.nasa.gov/apod/image/1710/MariusHills_LO2LRO_1080.jpg', function(data) {
+    console.log(data)
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+}) // jQ ready statement end
