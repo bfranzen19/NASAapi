5fbfa270f57b6e9dbc7b51048109a261c2608d1a
bt <btfranzen@gmail.com>
2017-10-26 08:37:41 -0600

    javascript working

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 // jQ ready statement start
-// var APIkey = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-10-18&end_date=2017-10-25&api_key=VonJt8Vve5LhNeSezacnaH3Fd7m0IPxnO5YI5Kr1'
-
 $(document).ready(function() {
   $.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-10-18&end_date=2017-10-25&api_key=VonJt8Vve5LhNeSezacnaH3Fd7m0IPxnO5YI5Kr1', function(allData) {
     // console.log(allData.near_earth_objects)
@@ -12,35 +10,46 @@ $(document).ready(function() {
         // accesses the days and checks if is_potentially_hazardous_asteroid is true
         if(allData.near_earth_objects[days][i].is_potentially_hazardous_asteroid === true) {
           dangerousAsteroids.push(allData.near_earth_objects[days][i])
-          // console.log(dangerousAsteroids[i].close_approach_data[0].miss_distance.miles)
         }
       }
     }
-  console.log(dangerousAsteroids)
-  for(var missDistance in dangerousAsteroids) {
-    missDistance = dangerousAsteroids[missDistance].close_approach_data[0].miss_distance.miles
-    // console.log(missDistance)
+
+  var astName = []
+  var astMissDistance = []
+  var astEstDiameter = []
+  var astVelocity = []
+
+  var makeObj = function() {
+    for(var j=0 ; j<dangerousAsteroids.length ; j++) {
+
+      astName.push(dangerousAsteroids[j].name)
+      astMissDistance.push(dangerousAsteroids[j].close_approach_data[0].miss_distance.miles)
+      astEstDiameter.push(dangerousAsteroids[j].estimated_diameter.feet.estimated_diameter_max)
+      astVelocity.push(dangerousAsteroids[j].close_approach_data[0].relative_velocity.miles_per_hour)
+
+      var scaryAsteroids = []
+      var asteroid = {
+        name: astName[j],
+        missDistance: astMissDistance[j],
+        estDiameter: astEstDiameter[j],
+        velocity: astVelocity[j],
+      }
+
+      scaryAsteroids.push(asteroid)
+      console.log(scaryAsteroids)
+    }
+  }
+  makeObj()
+
+  for(var i=0 ; i<dangerousAsteroids.length ; i++) {
+    console.log(asteroid[i])
+
   }
 
-  for(var estDiameter in dangerousAsteroids) {
-    estDiameter = dangerousAsteroids[estDiameter].estimated_diameter.feet.estimated_diameter_max
-    // console.log(estDiameter)
-  }
-
-  for(var velocity in dangerousAsteroids) {
-    velocity = dangerousAsteroids[velocity].close_approach_data[0].relative_velocity.miles_per_hour
-    // console.log(velocity)
-  }
 
 
-
-  // miss distance in miles
-  // console.log(dangerousAsteroids[0].close_approach_data[0].miss_distance.miles)
-  // estimated diameter in feet
-  // console.log(dangerousAsteroids[0].estimated_diameter.feet.estimated_diameter_max)
-  // velocity in mph
-  // console.log(dangerousAsteroids[0].close_approach_data[0].relative_velocity.miles_per_hour)
 
 
   })  // end of GET request
+
 }) // jQ ready statement end
