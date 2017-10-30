58796fb00950ae4cc01b3e358c999eea90fba43e
bt <btfranzen@gmail.com>
2017-10-29 22:51:29 -0600

    final commit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // jQ ready statement start
 $(document).ready(function() {
+  var start = '2017-17-10'
+  var end = '2017-10-25'
+
   $.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-10-18&end_date=2017-10-25&api_key=VonJt8Vve5LhNeSezacnaH3Fd7m0IPxnO5YI5Kr1', function(allData) {
     // console.log(allData.near_earth_objects)
     var dangerousAsteroids = []
@@ -18,6 +21,7 @@ $(document).ready(function() {
   var astMissDistance = []
   var astEstDiameter = []
   var astVelocity = []
+  var scaryAsteroids = []
 
   var makeObj = function() {
     for(var j=0 ; j<dangerousAsteroids.length ; j++) {
@@ -27,7 +31,6 @@ $(document).ready(function() {
       astEstDiameter.push(dangerousAsteroids[j].estimated_diameter.feet.estimated_diameter_max)
       astVelocity.push(dangerousAsteroids[j].close_approach_data[0].relative_velocity.miles_per_hour)
 
-      var scaryAsteroids = []
       var asteroid = {
         name: astName[j],
         missDistance: astMissDistance[j],
@@ -36,20 +39,34 @@ $(document).ready(function() {
       }
 
       scaryAsteroids.push(asteroid)
-      console.log(scaryAsteroids)
+
+      $('li').append(' name: ' + asteroid.name).append('<br>')
+
+      $('li').append(' miss distance: ' + asteroid.missDistance).append('<br>')
+
+      $('li').append(' estimated diameter: ' + asteroid.estDiameter).append('<br>')
+
+      $('li').append(' velocity: ' + asteroid.velocity).append('<br><br><br><br>')
     }
+
+
+
+
   }
+  // $('.results').append(makeObj())
   makeObj()
 
-  for(var i=0 ; i<dangerousAsteroids.length ; i++) {
-    console.log(asteroid[i])
-
-  }
 
 
+  $('.dates').text('start date: ' + start + ' to end date: ' + end)
 
-
+  var astLength = scaryAsteroids.length
+  $('.numRocks').text('number of scary rocks that got close to earth this week: ' + astLength)
 
   })  // end of GET request
-
 }) // jQ ready statement end
+
+
+
+// $('.results').append(' name: ' + asteroid.name, ' miss distance: ' + asteroid.missDistance, ' estimated diameter: ' + asteroid.estDiameter, ' velocity: ' + asteroid.velocity)
+// }
